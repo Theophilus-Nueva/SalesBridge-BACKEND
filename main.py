83dc3a0536411f5a60ee0861fe27318dbe7aca7c
Theophilus <theophilusnueva30@icloud.com>
2026-05-20 16:28:04 +0800

    blocked other links except the railway frontend

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://salesbridge-frontend-production.up.railway.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
